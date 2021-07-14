45c034ecb46c5242783614f6a86ead7ad17f9baa
dongchijoseph <huyblogging@gmail.com>
2021-07-14 18:05:06 +0700

    first

---
+++ main.rb
@@ -0,0 +1,10 @@
+# "X | O | X\n---------\nX | O | X\n---------\nX | O | X"
+
+class PlayerInput
+  @@default = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  private
+  def input(value)
+    if 
+
+
