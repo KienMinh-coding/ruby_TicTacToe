b21ef8f1f9b1c86eeeef57a2ac12dcca8e72074f
dongchijoseph <huyblogging@gmail.com>
2021-07-15 18:42:04 +0700

    input logic

--- main.rb
+++ main.rb
@@ -1,10 +1,24 @@
 # "X | O | X\n---------\nX | O | X\n---------\nX | O | X"
 
-class PlayerInput
-  @@default = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-  private
-  def input(value)
-    if 
+def get_player_input(board)
+  move = gets.chomp.to_i
+  check_player_input(move, board)
+end
 
+def check_player_input(move, board)
+  if board.include?(move) == true || Array(1..9).include?(move) == false
+    puts "You made a wrong move. Please try again"
+    get_player_input(board)
+  else
+    pass_input_to_board(move, board)
+  end
+end
 
+def pass_input_to_board(move, board)
+  board[move - 1] = move
+  puts board
+end
+
+get_player_input(board)
